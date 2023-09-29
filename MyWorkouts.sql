4a7c9ec2b9f88ce3ec06b985b94ec11371d64d5f
soniya1987 <vksoniya68@gmail.com>
2023-09-28 20:13:44 -0500

    Added work out file

---
+++ MyWorkouts.sql
@@ -0,0 +1,16 @@
+﻿--Create Table Employee
+--(
+--	EmployeeId int identity,
+--	FirstName varchar(255),
+--	LastName varchar(255),
+--	AddressLine varchar(255),
+--	City varchar(255)
+--);
+
+--select * from Employee;
+Insert into Employee (FirstName, LastName, AddressLine, City) 
+values ('Test','One','Test Address, Test Zone, 111111','Test City');
+select * from Employee;
+truncate table Employee;
+select * from Employee;
+Drop table Employee;
